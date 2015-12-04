52ae772f1cb1bce3a6bbd8ab9524469f4e07db76
AfuSensi <t.amatkarijo@gmail.com>
2015-12-04 12:28:04 +0100

    blocker mode changes

--- resources/[gameplay]/gus/admin_c.lua
+++ resources/[gameplay]/gus/admin_c.lua
@@ -1,27 +1,3 @@
-function theTool()
-	local mode = getResourceFromName'race' and getResourceState(getResourceFromName'race')=='running' and exports.race:getRaceMode()
-	if not (mode == "Sprint" or mode == "Never the same") or getElementData(localPlayer, 'markedblocker') then return end
-	for _, p in ipairs(getElementsByType'player') do
-		if getElementData(p, 'markedblocker') then
-			local vehMe = getPedOccupiedVehicle(localPlayer)
-			local vehBlocker = getPedOccupiedVehicle(p)
-			if vehMe and vehBlocker then
-				setElementCollidableWith(vehMe, vehBlocker, false)
-				setElementAlpha(vehBlocker, 140)
-			end
-		end
-	end
-end
-setTimer( theTool, 50, 0)
-
-function onClientExplosion()
-	if not (mode == "Sprint" or mode == "Never the same") or getElementData(localPlayer, 'markedblocker') then return end
-	if getElementType(source) == "player" and getElementData(p, 'markedblocker') then
-		cancelEvent()
-	end
-end
-addEventHandler('onClientExplosion', root, onClientExplosion)
-
 addEvent("onGameMessageSend2", true)
 addEventHandler("onGameMessageSend2", getRootElement(),
 function(text, lang)
