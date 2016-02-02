d1586f595e41e143bebfef94c60e9205907b5752
AfuSensi <t.amatkarijo@gmail.com>
2016-02-03 00:14:18 +0100

    Fix for duels
    Added "fake" spawnpoints.

--- resources/[race]/race_random/duel_s.lua
+++ resources/[race]/race_random/duel_s.lua
@@ -10,12 +10,32 @@ local function getAlivePlayers()
 	return players
 end
 
+    -- <spawnpoint id="spawnpoint (NRG-500) (1)" vehicle="522" team="none" posX="-932.79999" posY="-328" posZ="2.2" rotX="0" rotY="0" rotZ="270"></spawnpoint>
+    -- <spawnpoint id="spawnpoint (NRG-500) (2)" vehicle="522" team="none" posX="-315" posY="-328" posZ="2.2" rotX="0" rotY="0" rotZ="90"></spawnpoint>
+    local spawnInfo = {
+    {vehicle="522", team="none", posX=-932.79999, posY=-328, posZ=2.2, rotX=0, rotY=0, rotZ=270},
+    {vehicle="522", team="none", posX=-315, posY=-328, posZ=2.2, rotX=0, rotY=0, rotZ=90}
+}
+function createDuelSpawns()
+	for i=1,2 do
+		local spawnpoint = createElement( "duelspawn", "duelspawn"..i )
+		setElementParent( spawnpoint, resourceRoot )
+		for name,val in pairs(spawnInfo[i]) do
+			setElementData( spawnpoint, name, val )
+		end
+		setElementPosition( spawnpoint, spawnInfo[i].posX, spawnInfo[i].posY, spawnInfo[i].posZ )
+		setElementRotation( spawnpoint, spawnInfo[i].rotX, spawnInfo[i].rotY, spawnInfo[i].rotZ )
+	end
+end
+addEventHandler("onResourceStart",resourceRoot,createDuelSpawns)
+
+
 function duel(p, c, a)
 	local players = getAlivePlayers()
 	if #players ~= 2 or exports.race:getRaceMode() ~= "Destruction derby" then return end
 	
-	local map = getResourceMapRootElement(resource, "duel.map")
-	local spawns = getElementsByType('spawnpoint', map)
+	-- local map = getResourceMapRootElement(resource, "duel.map")
+	local spawns = getElementsByType('duelspawn', resourceRoot)
 	
 	for k, p in ipairs(players) do
 		local veh = getPedOccupiedVehicle(p)
