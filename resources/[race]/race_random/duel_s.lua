ad34435d0052057a54fae48d48fb68d3680c6c85
SDK <esdehka@gmail.com>
2016-02-07 10:37:34 +0100

    Fixes for duel
    #133

--- resources/[race]/race_random/duel_s.lua
+++ resources/[race]/race_random/duel_s.lua
@@ -1,3 +1,7 @@
+local maps = {
+	"duel.map",
+}
+
 local function getAlivePlayers()
 	local players = {}
 	for _, player in ipairs(getElementsByType('player')) do
@@ -5,17 +9,26 @@ local function getAlivePlayers()
 			table.insert(players, player)
 		end
 	end
-
-	table.sort(players,sortFunction)
 	return players
 end
 
-function duel(p, c, a)
+local mapname, maproot
+
+function startDuel(p, c, a)
 	local players = getAlivePlayers()
-	if #players ~= 2 or exports.race:getRaceMode() ~= "Destruction derby" then return end
+	if exports.race:getRaceMode() ~= "Destruction derby" then return outputChatBox("Not a DD map", p) end
+	if #players ~= 2 then return outputChatBox(#players .. " != 2 players for duel", p) end
 	
-	local map = getResourceMapRootElement(resource, "duel.map")
-	local spawns = getElementsByType('spawnpoint', map)
+	-- Load in random map
+	mapname = maps[math.random(#maps)]
+	local node = getResourceConfig ( mapname )
+	if not node then return outputChatBox("Couldn't load xml", p) end
+	maproot = loadMapData ( node, resourceRoot )
+	xmlUnloadFile ( node )
+	if not maproot then return outputChatBox("Couldn't load map", p) end
+	
+	-- Find spawns
+	local spawns = getElementsByType('spawnpoint', maproot)
 	
 	for k, p in ipairs(players) do
 		local veh = getPedOccupiedVehicle(p)
@@ -25,16 +38,17 @@ function duel(p, c, a)
 		setElementRotation(veh, 0, 0, getElementData(s, 'rotZ'))
 		setElementFrozen(veh, true)
 		setTimer(setElementFrozen, 100, 1, veh, false)
-		setElementDimension(map, 0)
 	end
 	triggerClientEvent('its_time_to_duel.mp3', resourceRoot)
 	outputChatBox("[DUEL] " .. getPlayerName(p) .. " #00FF00started a duel!", root, 0,255,0, true)
 end
-addCommandHandler('duel', duel, true)
+addCommandHandler('duel', startDuel, true)
 
 function stopDuel() 
-	local map = getResourceMapRootElement(resource, "duel.map")
-	setElementDimension(getResourceMapRootElement(resource, "duel.map"), 9001)
+	if maproot then
+		destroyElement(maproot)
+		maproot = nil
+	end
 end
 addEvent('stopDuel')
 addEventHandler('onPostFinish', root, stopDuel)
