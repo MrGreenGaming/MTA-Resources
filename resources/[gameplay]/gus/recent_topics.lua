3a24aad30a863077aa4d89dafba1f95211f0bd36
SDK <esdehka@gmail.com>
2016-03-05 14:13:22 +0100

    Fixed recent topics for new forum

--- resources/[gameplay]/gus/recent_topics.lua
+++ resources/[gameplay]/gus/recent_topics.lua
@@ -1,8 +1,8 @@
 --[[
-	Script that fetches the recent forum posts from L4G and shows them in chat
+	Script that fetches the recent forum posts from forum and shows them in chat
 --]]
 
-local url = "http://mrgreengaming.com/forums/forum/31-multi-theft-auto/";
+local url = "https://mrgreengaming.com/forums/forum/31-multi-theft-auto.xml";
 local showTopics = 4;
 local interval = 3 * 60 * 60 * 1000;
 local chatDelay = 10 * 1000;
@@ -35,38 +35,31 @@ function fetchCallback ( data, errno, ... )
 		outputDebugString("fetchRemote error: " .. tostring(errno) .. ' (' .. type(errno) .. ')');
 		return error();
 	end
-	data = split ( data, "\n" );
-	local x = -1;
-	local title = "";
-	local pinned = false
-	outputChatBox("Recent MTA subforum topics on Mr.Green", root, 0, 255, 0 );
-	for _, line in ipairs ( data ) do
-		if (string.find(line, "Pinned")) then
-			pinned = true
-		elseif (string.find(line, "topic_title")) then
-			if not pinned then
-				x = x + 1;
-				if x > 0 then
-					title = string.match(unescape(line), "title='(.+) %- started");
-					author = string.gsub(string.match(unescape(data[_+6]), "(<a .*</a>)"), "<.->", "")
-					outputChatBox ( '* ' .. title .. ' (' .. author .. ')', root, 0, 255, 0 );
-				end
-			else
-				pinned = false
-			end
-		end
-		if (x >= showTopics) then
-			return;
-		end
-	end
-end
 
--- http://stackoverflow.com/questions/14899734/unescape-numeric-xml-entities-with-lua
-local gsub, char = string.gsub, string.char
-local entityMap  = {["lt"]="<",["gt"]=">",["amp"]="&",["quot"]='"',["apos"]="'"}
-local entitySwap = function(orig,n,s)
-  return entityMap[s] or n=="#" and char(s) or orig
-end
-function unescape(str)
-  return gsub( str, '(&(#?)([%d%a]+);)', entitySwap )
+	outputChatBox("Recent MTA subforum topics on Mr.Green", root, 0, 255, 0 );
+	if data then
+		local f = fileCreate'recent.xml'
+		fileWrite(f, data)
+		fileClose(f)
+		local x = xmlLoadFile'recent.xml'
+		if x then
+			local c = xmlFindChild(x, 'channel', 0)
+			if c then
+				for i=1,showTopics do
+					local item = xmlFindChild(c, 'item', i-1)
+					if item then
+						local title = xmlFindChild(item, 'title', 0)
+						-- outputChatBox ( '* ' .. xmlNodeGetValue(title) .. ' (' .. author .. ')', root, 0, 255, 0 );
+						outputChatBox ( '* ' .. xmlNodeGetValue(title), root, 0, 255, 0 );
+					else
+						break
+					end
+				end
+			end
+			xmlUnloadFile(x)
+		end
+		fileDelete'recent.xml'
+	else
+		outputDebugString'recent forum topics error'
+	end
 end
