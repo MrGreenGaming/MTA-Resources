94273853442c65c42b977255042764ccf5ad949d
AleksCore <dr.aleks.freeman@gmail.com>
2016-09-23 23:39:08 +0300

    RTF: Max map duration set to 10 minutes

--- resources/[race]/race/modes/rtf.lua
+++ resources/[race]/race/modes/rtf.lua
@@ -158,4 +158,5 @@ RTF.modeOptions = {
 	autopimp_map_can_override = false,
 	ghostmode = true,
 	ghostmode_map_can_override = false,
+	duration = (g_MapOptions and (g_MapOptions.duration < 600)) or 10*60*1000,
 }
