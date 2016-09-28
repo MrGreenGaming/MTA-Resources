1ff67ee596b8264cae91bf49a66b4ed69075b4f6
AleksCore <dr.aleks.freeman@gmail.com>
2016-09-28 22:18:59 +0300

    Fix for GC text popups (negative numbers)
    Now if GC was taken from a player for buying anything - text popup will
    be shown correctly and red-colored.
    Example:
    - player got 100 GC - text: "+100 GC" green-colored
    - players lost GC (-100 GC) - text: "-100 GC" red-colored

--- resources/[gameplay]/gc/gc_effects_c.lua
+++ resources/[gameplay]/gc/gc_effects_c.lua
@@ -1,16 +1,36 @@
 local screenW, screenH = guiGetScreenSize()
 
+local t = {
+				["+"] = {
+						r = 0,
+						g = 255,
+						b = 0,
+						},
+				["-"] = {
+						r = 255,
+						g = 0,
+						b = 0,
+						},
+}
+
 local g_GCText = {}
 
 function GCTextPopUp( amount )
 	local gc = amount or 1
+	
+	local symbol = "+"
+	if gc < 0 then
+		symbol = "-"
+	end
+	
 	local startTime = getTickCount()
 	local data = {
 				["startInterp"] = {0.5, 255, 0},
 				["startTime"] = startTime,
 				["endInterp"] = {0.3, 0, 0},
 				["endTime"] = startTime + 2000,
-				["gc"] = gc
+				["gc"] = math.abs(gc),
+				["symbol"] = symbol,
 				}
 				
 	table.insert(g_GCText, data)
@@ -34,7 +54,8 @@ function popGCTextUp()
 		h2, a2, z2, 
 		progress, "Linear")
  
-		dxDrawText("+"..v.gc.." GC", 0, h*screenH, screenW, screenH, tocolor(0, 255, 0, a), 1, "bankgothic", "center", "top", false, false, true, false, true)
+		local s = v.symbol
+		dxDrawText(v.symbol..""..v.gc.." GC", 0, h*screenH, screenW, screenH, tocolor(t[s].r, t[s].g, t[s].b, a), 1, "bankgothic", "center", "top", false, false, true, false, true)
 		
 		if now >= v.endTime then
 			table.remove (g_GCText, i)
