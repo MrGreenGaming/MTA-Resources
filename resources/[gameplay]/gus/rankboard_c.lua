d773424bed07e5adae6064b038c21c074d744d0d
Simeon (Moni) Mihailov <spiderman07@abv.bg>
2016-11-14 00:32:05 +0200

    Forgot to add RGBtoHex function xd
    It is 0 30 ffs :D

--- resources/[gameplay]/gus/rankboard_c.lua
+++ resources/[gameplay]/gus/rankboard_c.lua
@@ -474,3 +474,14 @@ function getPlayerFinishInfo(name)
 	end
 	return false, false
 end
+
+function RGBToHex(red, green, blue, alpha)
+	if((red < 0 or red > 255 or green < 0 or green > 255 or blue < 0 or blue > 255) or (alpha and (alpha < 0 or alpha > 255))) then
+		return nil
+	end
+	if(alpha) then
+		return string.format("#%.2X%.2X%.2X%.2X", red,green,blue,alpha)
+	else
+		return string.format("#%.2X%.2X%.2X", red,green,blue)
+	end
+end
