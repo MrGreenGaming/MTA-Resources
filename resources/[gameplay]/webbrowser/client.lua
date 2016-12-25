0d341a49fbd53afbae1389c13c13217928b34264
SDK <esdehka@gmail.com>
2016-12-25 17:08:50 +0100

    Added webbrowser to forum

---
+++ resources/[gameplay]/webbrowser/client.lua
@@ -0,0 +1,26 @@
+screenWidth, screenHeight = guiGetScreenSize()
+
+--[[
+addEventHandler("onClientResourceStart", resourceRoot,
+	function()
+		-- Register bind for toggling the mouse cursor
+		bindKey("lalt", "down", function(button, state) showCursor(not isCursorShowing()) end)
+		
+		-- Create Chromium icon
+		local icon = GuiStaticImage(0, screenHeight - 48, 48, 48, "chromiumIcon.png", false)
+		addEventHandler("onClientGUIClick", icon,
+			function(button, state)
+				if button == "left" and state == "up" then
+					-- Create the browser now
+					if WebBrowserGUI.instance ~= nil then return end
+					WebBrowserGUI.instance = WebBrowserGUI:new()
+				end
+			end
+		)
+	end
+)
+--]]
+addCommandHandler('mrgreen', function()
+	if WebBrowserGUI.instance ~= nil then return end
+	WebBrowserGUI.instance = WebBrowserGUI:new("https://mrgreengaming.com/forums/topic/19257-merry-christmas/")
+end)
