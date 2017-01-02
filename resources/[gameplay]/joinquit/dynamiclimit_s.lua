c502c034a3f18c213b8eb59a4760a3273e561240
SDK <esdehka@gmail.com>
2017-01-02 04:09:09 +0100

    Dynamic limit
    For #294

---
+++ resources/[gameplay]/joinquit/dynamiclimit_s.lua
@@ -0,0 +1,13 @@
+local minimumFreeSlots = 5		-- minimum amount of slots that will seem available, if less slots than this are available, the limit will be incremented ( eg if 5 then 36/40 -> 36/50)
+local increments = 10			-- multiplier for server slots (eg if 10 then /10 /20 /30 /40 ...)
+local minimumSlots = 20			-- dont go lower than this (eg if 20 then 4/10 -> 4/20)
+
+function join()
+	local players = getPlayerCount()
+	local newLimit = math.max(minimumSlots, math.ceil((players+minimumFreeSlots)/increments)*increments)
+	if getMaxPlayers() ~= newLimit then
+		setMaxPlayers(newLimit)
+		outputDebugString('New slots limit ' .. newLimit)
+	end
+end
+addEventHandler('onPlayerJoin', root, join)
