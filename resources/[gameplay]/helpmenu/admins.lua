18247691d05d8c562e4661bc3fce6a615634cd2e
SDK <EsDeKa@users.noreply.github.com>
2017-03-19 23:44:00 +0100

    Add Santiii to admins list

--- resources/[gameplay]/helpmenu/admins.lua
+++ resources/[gameplay]/helpmenu/admins.lua
@@ -11,5 +11,6 @@
 	{ name = "Stig", title = "Old Admin", accountName = "Stig" },
 	{ name = "Hulpje", title = "Dota Admin", accountName = "hulpje" },
 	{ name = "Besweeet", title = "4th Admin", accountName = "besweeet" },
-	{ name = "Bierbuikje", title = "NIU Admin", accountName = "bierbuikje" }
+	{ name = "Bierbuikje", title = "NIU Admin", accountName = "bierbuikje" },
+	{ name = "Santiii733", title = "Re-Volt Master", accountName = "santiii733" },
 }
