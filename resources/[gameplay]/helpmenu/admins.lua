d0be56ed01927836b8fc74780cbf74e80d1cb842
Bierbuikje <hans_swart@hotmail.nl>
2017-05-07 20:01:14 +0200

    Added r0cK
    r0cK

--- resources/[gameplay]/helpmenu/admins.lua
+++ resources/[gameplay]/helpmenu/admins.lua
@@ -7,10 +7,11 @@ admins = {
 	{ name = "MoshPit", title = "Map Manager", accountName = "MoshMosh" },
 	{ name = "neox.", title = "Map Assistant", accountName = "neox" },
 	{ name = "Goldberg", title = "Map Assistant", accountName = "Goldberg" },
+	{ name = "r0cK", title = "Event Admin", accountName = "r0cK" },
 	{ name = "Jack123", title = "Mother Russia", accountName = "Jack123" },
 	{ name = "Stig", title = "Old Admin", accountName = "Stig" },
 	{ name = "Hulpje", title = "Dota Admin", accountName = "hulpje" },
 	{ name = "Besweeet", title = "4th Admin", accountName = "besweeet" },
 	{ name = "Bierbuikje", title = "NIU Admin", accountName = "bierbuikje" },
-	{ name = "Santiii733", title = "Re-Volt Master", accountName = "santiii733" },
+	{ name = "Santiii733", title = "Re-Volt Master", accountName = "santiii733" }
 }
