925f015cfb2961ca931b236a410926cc38458b62
f1madkiller <ben.cool1@hotmail.co.uk>
2017-06-24 19:43:51 +0100

    changed bier to developer and made flipper admin
    changed bier to developer and made flipper admin and hopefully this PR works correctly this time :)

--- resources/[gameplay]/helpmenu/admins.lua
+++ resources/[gameplay]/helpmenu/admins.lua
@@ -1,7 +1,7 @@
 admins = {
 	{ name = "Ywa", title = "Founder/Owner", accountName = "Ywa" },
-	{ name = "Flipper", title = "Manager", accountName = "KoM|Flipper" },
-	{ name = "FantomicanYoshi", title = "Manager", accountName = "BlueYoshi97" },        
+	{ name = "FantomicanYoshi", title = "Manager", accountName = "BlueYoshi97" }, 
+	{ name = "Bierbuikje", title = "developer", accountName = "bierbuikje" },
 	{ name = "Bob_Taylor", title = "Map Manager", accountName = "Bob_Taylor" },
 	{ name = "Moh", title = "Map Manager", accountName = "Moh" },
 	{ name = "MoshPit", title = "Map Manager", accountName = "MoshMosh" },
@@ -12,6 +12,6 @@ admins = {
 	{ name = "Stig", title = "Old Admin", accountName = "Stig" },
 	{ name = "Hulpje", title = "Dota Admin", accountName = "hulpje" },
 	{ name = "Besweeet", title = "4th Admin", accountName = "besweeet" },
-	{ name = "Bierbuikje", title = "NIU Admin", accountName = "bierbuikje" },
 	{ name = "Santiii733", title = "Re-Volt Master", accountName = "santiii733" }
+	{ name = "Flipper", title = "Admin", accountName = "KoM|Flipper" },
 }
