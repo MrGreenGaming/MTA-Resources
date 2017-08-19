ba0f56abad8021b33737322d03e00881f83a1b82
Bierbuikje <hans_swart@hotmail.nl>
2017-08-19 12:51:39 +0200

    Update admins.lua

--- resources/[gameplay]/helpmenu/admins.lua
+++ resources/[gameplay]/helpmenu/admins.lua
@@ -1,18 +1,17 @@
 admins = {
 	{ name = "Ywa", title = "Founder/Owner", accountName = "Ywa" },
+	{ name = "Cena", title = "Server Manager", accountName = "Cena" },
 	{ name = "Bierbuikje", title = "Developer", accountName = "bierbuikje" },
 	{ name = "Bob_Taylor", title = "Map Manager", accountName = "Bob_Taylor" },
-	{ name = "Cena", title = "Map Manager", accountName = "Cena" },
 	{ name = "MoshPit", title = "Map Manager", accountName = "MoshMosh" },
 	{ name = "neox.", title = "Map Assistant", accountName = "neox" },
 	{ name = "Goldberg", title = "Map Assistant", accountName = "Goldberg" },
 	{ name = "F1MADKILLER", title = "Mad Admin", accountName = "F1madkiller" },
-	{ name = "r0cK", title = "Event Admin", accountName = "r0cK" },
 	{ name = "Jack123", title = "Mother Russia", accountName = "Jack123" },
 	{ name = "Stig", title = "Old Admin", accountName = "Stig" },
 	{ name = "Hulpje", title = "Dota Admin", accountName = "hulpje" },
 	{ name = "Besweeet", title = "4th Admin", accountName = "besweeet" },
 	{ name = "Santiii733", title = "Re-Volt Master", accountName = "santiii733" },
 	{ name = "Flipper", title = "Admin", accountName = "KoM|Flipper" },
-	{ name = "SkyNet", title = "Admin", accountName = "SkyNet" },
+	{ name = "SkyNet", title = "Admin", accountName = "SkyNet" }
 }
