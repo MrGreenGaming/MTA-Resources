c50214da48625272252aa807bbcb7fe7788b491f
Bierbuikje <hans_swart@hotmail.nl>
2017-08-27 20:22:18 +0200

    Update admins

--- resources/[gameplay]/helpmenu/admins.lua
+++ resources/[gameplay]/helpmenu/admins.lua
@@ -4,6 +4,7 @@ admins = {
 	{ name = "Bierbuikje", title = "Developer", accountName = "bierbuikje" },
 	{ name = "Bob_Taylor", title = "Map Manager", accountName = "Bob_Taylor" },
 	{ name = "MoshPit", title = "Map Manager", accountName = "MoshMosh" },
+	{ name = "SkyNet", title = "Map Manager", accountName = "SkyNet" },
 	{ name = "neox.", title = "Map Assistant", accountName = "neox" },
 	{ name = "Goldberg", title = "Map Assistant", accountName = "Goldberg" },
 	{ name = "F1MADKILLER", title = "Mad Admin", accountName = "F1madkiller" },
@@ -12,6 +13,5 @@ admins = {
 	{ name = "Hulpje", title = "Dota Admin", accountName = "hulpje" },
 	{ name = "Besweeet", title = "4th Admin", accountName = "besweeet" },
 	{ name = "Santiii733", title = "Re-Volt Master", accountName = "santiii733" },
-	{ name = "Flipper", title = "Admin", accountName = "KoM|Flipper" },
-	{ name = "SkyNet", title = "Admin", accountName = "SkyNet" }
+	{ name = "Flipper", title = "Admin", accountName = "KoM|Flipper" }
 }
