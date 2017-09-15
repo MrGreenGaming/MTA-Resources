7bb2f6639498593d5744fba84ff859d36ec301d2
Bierbuikje <hans_swart@hotmail.nl>
2017-09-15 22:00:05 +0200

    Teamwar fix 7

--- resources/[gameplay]/gcshop/teams/teams_s.lua
+++ resources/[gameplay]/gcshop/teams/teams_s.lua
@@ -631,13 +631,10 @@ function sendTWRequest(challengerPlayer, arg)
 	end
 	
 	local challengerTeamName
+	local victimTeamName
 	for a,b in ipairs(teams) do
 		if tostring(b.teamid) == tostring(challengerTeam) then challengerTeamName = b.name end
-	end
-	
-	local victimTeamName
-	for a,b in ipairs(t) do
-		if tostring(b[1]) == tostring(victimTeam) then victimTeamName = b[3] end
+		if tostring(b.teamid) == tostring(victimTeam) then victimTeamName = b.name end
 	end
 	
 	local victimForumID
