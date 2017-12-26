164fc5549ef362c29461c7bb99dbe1689278ad7e
Aleksey <dr.aleks.freeman@gmail.com>
2017-12-26 19:33:37 +0300

    Fixed MADKILLER's account name
    As he did it wrong (silly MADKILLER)

--- resources/[gameplay]/helpmenu/admins.lua
+++ resources/[gameplay]/helpmenu/admins.lua
@@ -6,7 +6,7 @@ admins = {
 	{ name = "MoshPit", title = "Map Manager", accountName = "MoshMosh" },
 	{ name = "Goldberg", title = "Map Manager", accountName = "Goldberg" },
 	{ name = "neox.", title = "Map Assistant", accountName = "neox" },
-	{ name = "F1MADKILLER", title = "Map Assistant", accountName = "F1mad" },
+	{ name = "F1MADKILLER", title = "Map Assistant", accountName = "f1mad" },
 	{ name = "SkyNet", title = "Admin", accountName = "SkyNet" },
 	{ name = "Jack123", title = "Mother Russia", accountName = "Jack123" },
 	{ name = "Stig", title = "Old Admin", accountName = "Stig" },
