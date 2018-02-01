40a271c8e2d0cc08e2ba56802a2105773195534d
Bierbuikje <hans_swart@hotmail.nl>
2018-02-01 11:02:24 +0100

    Wololo update #13

--- resources/[gameplay]/gcshop/horns/horns_s.lua
+++ resources/[gameplay]/gcshop/horns/horns_s.lua
@@ -240,13 +240,14 @@ end
 
 function playerUsingHorn_s(horn,car)
 	if getElementData(source, "state") == "alive" and getPedOccupiedVehicle(source) then
-		if tonumber(horn) == 389 then -- Wololo horn from Age of Empires 2. Changes target's vehicle color to the color of the source's vehicle.
+		if tonumber(horn) == 389 then -- Wololo horn from Age of Empires 2. Changes target's vehicle color to the color of the source's vehicle. Adds opponent to source's team for 5 minutes.
 			local c = {}
 			local d = {} -- delta
 			local s = {} -- source
 			local t = {} -- target
 			local dist = 15
 			local dis = {dist, false}
+			local teamSwitches = {}
 			
 			local p = getElementsByType("player")
 			local dim = getElementData(source, "dim")
@@ -269,8 +270,34 @@ function playerUsingHorn_s(horn,car)
 			end
 			
 			if dis[1] < dist and getPedOccupiedVehicle(dis[2]) and getElementData(dis[2], "state") == "alive" then
-				outputDebugString("Player: " .. getPlayerNametagText(dis[2]) .. " is the nearest on " .. dis[1] .. " distance. Update #12", 0)
-				setTimer(function() setVehicleColor(getPedOccupiedVehicle(dis[2]), c[1], c[2], c[3], c[4], c[5], c[6], c[7], c[8], c[9], c[10], c[11], c[12]) end, 1515, 1)
+				outputDebugString("Player: " .. getPlayerNametagText(dis[2]) .. " is the nearest on " .. dis[1] .. " distance. Update #13", 0)
+				--setTimer(function()
+				
+				setVehicleColor(getPedOccupiedVehicle(dis[2]), c[1], c[2], c[3], c[4], c[5], c[6], c[7], c[8], c[9], c[10], c[11], c[12])
+				
+				local bool = true
+				for e,f in ipairs(teamSwitches) do if f[1] == dis[2] then bool = false end end
+				
+				if bool then
+					local teamSource = getPlayerTeam(source)
+					local teamTarget = getPlayerTeam(dis[2])
+					if teamSource == false then teamSource = nil end
+					if teamTarget == false then teamTarget = nil end
+					
+					table.insert(teamSwitches,{dis[2],teamTarget})
+					setPlayerTeam(dis[2], teamSource)
+					
+					setTimer(function()
+						for c,d in ipairs(teamSwitches) do
+							if d[1] == dis[2] then
+								setPlayerTeam(d[1], d[2])
+								table.remove(teamSwitches,c)
+							end
+						end
+					end, 1000*60*5, 1)
+				end
+				
+				--end, 1515, 1)
 			end
 		end
 	end	
