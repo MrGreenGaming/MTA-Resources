43f70a81302138fd9ee8e05bfae7dedf9da2c9dd
Bierbuikje <hans_swart@hotmail.nl>
2018-02-02 17:01:44 +0100

    Wololo update #19

--- resources/[gameplay]/gcshop/horns/horns_s.lua
+++ resources/[gameplay]/gcshop/horns/horns_s.lua
@@ -240,7 +240,7 @@ end
 
 function playerUsingHorn_s(horn,car)
 	if getElementData(source, "state") == "alive" and getPedOccupiedVehicle(source) then
-		local gamemode = getResourceFromName(exports.mapmanager:getRunningGamemode())
+		local gamemode = getResourceName(exports.mapmanager:getRunningGamemode())
 		outputDebugString("Player: " .. getPlayerNametagText(source) .. " used horn " .. horn .. " in gamemode " .. gamemode, 0)
 		
 		if tonumber(horn) == 389 and not (gamemode == "ctf") then -- Wololo horn from Age of Empires 2. Changes target's vehicle color to the color of the source's vehicle. Adds opponent to source's team for 5 minutes.
@@ -273,7 +273,7 @@ function playerUsingHorn_s(horn,car)
 			end
 			
 			if dis[1] < dist and getPedOccupiedVehicle(dis[2]) and getElementData(dis[2], "state") == "alive" then
-				--outputDebugString("Player: " .. getPlayerNametagText(dis[2]) .. " is the nearest on " .. dis[1] .. " distance. Update #18", 0)
+				--outputDebugString("Player: " .. getPlayerNametagText(dis[2]) .. " is the nearest on " .. dis[1] .. " distance. Update #19", 0)
 				--setTimer(function()
 				
 				setVehicleColor(getPedOccupiedVehicle(dis[2]), c[1], c[2], c[3], c[4], c[5], c[6], c[7], c[8], c[9], c[10], c[11], c[12])
