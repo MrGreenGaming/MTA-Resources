04403ea50b71f9c5279384ea21264eb1d8410e54
Bierbuikje <hans_swart@hotmail.nl>
2018-02-08 22:16:06 +0100

    Censorship resource addon

--- resources/[admin]/censorship/censorship_s.lua
+++ resources/[admin]/censorship/censorship_s.lua
@@ -12,20 +12,7 @@ end
 addEventHandler('onResourceStart', getResourceRootElement(),resourceStart)
 
 function onPlayerChat_s(msg, msgType)
-	msg = string.gsub(msg, "#%x%x%x%x%x%x", "")
-	msg = string.lower(msg)
-	for a,b in ipairs(ads) do b = string.lower(b) end
-	
-	local bool = false
-	for a,b in ipairs(ads) do
-		local s,e = string.find(msg, b)
-		if not (s == nil) and not (e == nil) then
-			bool = true
-			break
-		end
-	end
-	
-	if not bool then return end
+	if not isBanMsg(msg) then return end
 	cancelEvent()
 	local playerName = string.gsub(getPlayerName(source), "#%x%x%x%x%x%x", "")
 	outputServerLog("[CANCELED]CHAT: " .. getPlayerName(source) .. ": " .. msg)
@@ -39,3 +26,25 @@ function onPlayerChat_s(msg, msgType)
 	outputDebugString("Banned player: " .. playerName .. " IP: " .. ip .. " Serial: " .. serial,3)
 end
 addEventHandler("onPlayerChat", root, onPlayerChat_s)
+
+function isBanMsg(msg)
+	msg = string.gsub(msg, "#%x%x%x%x%x%x", "")
+	msg = string.lower(msg)
+	for a,b in ipairs(ads) do b = string.lower(b) end
+	
+	for a,b in ipairs(ads) do
+		local s,e = string.find(msg, b)
+		if not (s == nil) and not (e == nil) then
+			return true
+		end
+	end
+	
+	return false
+end
+
+function isBlockedMsg(msg)
+	if isBanMsg(msg) then
+		return true
+	end
+	return false
+end
