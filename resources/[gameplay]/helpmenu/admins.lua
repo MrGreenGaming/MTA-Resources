c11d1952fd12759bdda6676217954874fd51ff59
f1madkiller <ben.cool1@hotmail.co.uk>
2018-06-14 21:41:48 +0100

    Removed haze added dub

--- resources/[gameplay]/helpmenu/admins.lua
+++ resources/[gameplay]/helpmenu/admins.lua
@@ -5,10 +5,10 @@ admins = {
 	{ name = "Bob_Taylor", title = "Map Manager", accountName = "Bob_Taylor" },
 	{ name = "MoshPit", title = "Map Manager", accountName = "MoshMosh" },
 	{ name = "F1MADKILLER", title = "Map Manager", accountName = "f1mad" },
-	{ name = "Haze", title = "Map Assistant", accountName = "renopopper" },
 	{ name = "Jack123", title = "Mother Russia", accountName = "Jack123" },
 	{ name = "Stig", title = "Old Admin", accountName = "Stig" },
 	{ name = "Santiii733", title = "Discord Admin", accountName = "santiii733" },
 	{ name = "Maher", title = "Sheikh", accountName = "maher" },
-	{ name = "Abrahax", title = "Kadabrahax", accountName = "abrahax" },
+	{ name = "Abrahax", title = "Kadabrahax", accountName = "abrahax" }
+	{ name = "DubStep", title = "Niu Admin", accountName = "michael" },
 }
