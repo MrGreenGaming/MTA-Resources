b5b43606276788e3ca687d435e5f8fa9e3da5a37
f1madkiller <ben.cool1@hotmail.co.uk>
2018-06-28 12:21:06 +0100

    Update admin list (#415)

--- resources/[gameplay]/helpmenu/admins.lua
+++ resources/[gameplay]/helpmenu/admins.lua
@@ -1,14 +1,14 @@
 admins = {
-	{ name = "Ywa", title = "Founder/Owner", accountName = "Ywa" },
+	{ name = "Ywa", title = "Founder", accountName = "Ywa" },
 	{ name = "Cena", title = "Server Manager", accountName = "Cena" },
 	{ name = "Bierbuikje", title = "Developer", accountName = "bierbuikje" },
 	{ name = "Bob_Taylor", title = "Map Manager", accountName = "Bob_Taylor" },
 	{ name = "MoshPit", title = "Map Manager", accountName = "MoshMosh" },
 	{ name = "F1MADKILLER", title = "Map Manager", accountName = "f1mad" },
-	{ name = "Haze", title = "Map Assistant", accountName = "renopopper" },
 	{ name = "Jack123", title = "Mother Russia", accountName = "Jack123" },
 	{ name = "Stig", title = "Old Admin", accountName = "Stig" },
 	{ name = "Santiii733", title = "Discord Admin", accountName = "santiii733" },
 	{ name = "Maher", title = "Sheikh", accountName = "maher" },
 	{ name = "Abrahax", title = "Kadabrahax", accountName = "abrahax" },
+	{ name = "DubStep", title = "Niu Admin", accountName = "michael" },
 }
