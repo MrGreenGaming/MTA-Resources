c82594b7ac41bfe55b787da50f19b943c6231b16
Jarno <jarno@limetric.com>
2018-07-13 13:23:29 +0200

    Possible fix for broken voice perk (#418)

--- resources/[gameplay]/gcshop/items/voice/gcvoice_s.lua
+++ resources/[gameplay]/gcshop/items/voice/gcvoice_s.lua
@@ -6,7 +6,7 @@ local ID = 5
 
 function loadGCVoice ( player, bool, settings )
 	if bool then
-		setElementData(source, 'bCanUseVoice', true)
+		setElementData(player, 'bCanUseVoice', true)
 	else
 		setElementData(player, 'bCanUseVoice', nil)
 	end
