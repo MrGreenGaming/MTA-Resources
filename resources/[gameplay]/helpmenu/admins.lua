92b26eb3c436c33d98190cfbe535f5f8850f26e8
Cena <mohammed3103@gmail.com>
2018-09-30 18:07:17 +0200

    Updated list (#449)

--- resources/[gameplay]/helpmenu/admins.lua
+++ resources/[gameplay]/helpmenu/admins.lua
@@ -4,11 +4,12 @@ admins = {
 	{ name = "Bierbuikje", title = "Developer", accountName = "bierbuikje" },
 	{ name = "Bob_Taylor", title = "Map Manager", accountName = "Bob_Taylor" },
 	{ name = "MoshPit", title = "Map Manager", accountName = "MoshMosh" },
-	{ name = "F1MADKILLER", title = "Map Manager", accountName = "f1mad" },
 	{ name = "Jack123", title = "Mother Russia", accountName = "Jack123" },
 	{ name = "Stig", title = "Old Admin", accountName = "Stig" },
 	{ name = "Santiii733", title = "Discord Admin", accountName = "santiii733" },
 	{ name = "Maher", title = "Sheikh", accountName = "maher" },
 	{ name = "Abrahax", title = "Kadabrahax", accountName = "abrahax" },
 	{ name = "DubStep", title = "Niu Admin", accountName = "michael" },
+	{ name = "P0L", title = "Silent Admin", accountName = "P0L" },
+	
 }
