96349729b478fe44a6696f8f46b78a4ac2b8ada3
Santiii733 <Santiii727@users.noreply.github.com>
2018-10-17 01:58:01 -0300

    Update admins.lua

--- resources/[gameplay]/helpmenu/admins.lua
+++ resources/[gameplay]/helpmenu/admins.lua
@@ -4,12 +4,12 @@ admins = {
 	{ name = "Bierbuikje", title = "Developer", accountName = "bierbuikje" },
 	{ name = "Bob_Taylor", title = "Map Manager", accountName = "Bob_Taylor" },
 	{ name = "MoshPit", title = "Map Manager", accountName = "MoshMosh" },
+	{ name = "Dubby", title = "Map Manager", accountName = "michael" },
+	{ name = "P0L", title = "Map Assistant", accountName = "P0L" },
 	{ name = "Jack123", title = "Mother Russia", accountName = "Jack123" },
 	{ name = "Stig", title = "Old Admin", accountName = "Stig" },
 	{ name = "Santiii733", title = "Discord Admin", accountName = "santiii733" },
 	{ name = "Maher", title = "Sheikh", accountName = "maher" },
 	{ name = "Abrahax", title = "Kadabrahax", accountName = "abrahax" },
-	{ name = "Dubby", title = "Quality Onion", accountName = "michael" },
-	{ name = "P0L", title = "Silent Admin", accountName = "P0L" },
 	
 }
