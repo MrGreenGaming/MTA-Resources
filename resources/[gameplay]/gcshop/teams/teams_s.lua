cec6dbb78d2fb46e10038ef36ece5b8c3e8562c6
Mihoje <mihasoftware@gmail.com>
2019-01-15 16:38:47 +0100

    Team noname fix try No. 2
    I can't test this, I'm working blind on this please bear with me. Again, I'm gonna revert if something major breaks.

--- resources/[gameplay]/gcshop/teams/teams_s.lua
+++ resources/[gameplay]/gcshop/teams/teams_s.lua
@@ -11,9 +11,25 @@ addEventHandler('onShopInit', root, function()
 end)
 
 addEventHandler("onGCShopLogin", root, function()
-    checkPlayerTeam(source, true)
-    triggerClientEvent(source, "teamLogin", resourceRoot)
+	local fid = exports.gc:getPlayerForumID(source)
+	dbQuery(nameCache, {source, forumid}, handlerConnect, [[SELECT * FROM gc_nickcache WHERE forumid=?]], fid)
+	
+	triggerClientEvent(source, "teamLogin", resourceRoot)
+	checkPlayerTeam(player, true)
 end)
+
+function nameCache(qh, player, forumid)
+	local result = dbPoll(qh, -1)
+	local pName = getPlayerName(player)
+	if #result == 0 then
+		dbExec(handlerConnect, [[INSERT INTO gc_nickcache(forumid, name) VALUES (?,?)]], forumid, pName)
+	else
+		local row = result[1]
+		if row.name ~= pName then
+			dbExec(handlerConnect, [[UPDATE gc_nickcache SET name=? WHERE forumid=?]], pName, forumid)
+		end
+	end
+end
 
 addEventHandler("onGCShopLogout", root, function()
     playerteams[source] = nil
@@ -115,6 +131,50 @@ function checkPlayerTeam(player, bLogin)
     dbQuery(checkPlayerTeam2, { player, bLogin }, handlerConnect, [[SELECT * FROM `team_members` INNER JOIN `team` ON `team_members`.`teamid` = `team`.`teamid` ORDER BY `team`.`create_timestamp`, `join_timestamp`]])
 end
 
+function sendClientData(nicks, res, player, r)
+	local result = res
+	local fid
+	local c = false
+	local forumNameTable = {}
+	for i, row in ipairs(result) do
+		fid = row.forumid
+		
+		for j, r in ipairs(nicks) do
+			if row.forumid == r.forumid then
+				row.mta_name = r.name:gsub("#%x%x%x%x%x%x", "")
+				c = true
+				break
+			end
+		end
+		
+		if not c then
+			table.insert(forumNameTable, {userId=fid})
+		end
+		c = false
+	end
+	
+	if #forumNameTable > 0 then
+		exports.gc:getMultipleForumAccountDetails(forumNameTable, function(resp)
+			if not resp then
+				triggerClientEvent('teamsData', resourceRoot, result, player, r)
+			else
+				for _, p in ipairs(resp) do
+					for _, row in ipairs(result) do
+						if row.forumid == p.forumid then
+							row.mta_name = p.name
+							break
+						end
+					end
+				end
+				triggerClientEvent('teamsData', resourceRoot, result, player, r)
+			end
+		end)
+		return
+	end
+	
+	triggerClientEvent('teamsData', resourceRoot, result, player, r)
+end
+
 function checkPlayerTeam2(qh, player, bLogin)
     local result = dbPoll(qh, -1)
 
@@ -144,8 +204,12 @@ function checkPlayerTeam2(qh, player, bLogin)
         r.age = string.format("%.2f", age / (24 * 60 * 60))
         outputConsole('[TEAMS] Team days left: ' .. r.age, player)
     end
-
-    triggerClientEvent('teamsData', resourceRoot, result, player, r)
+	
+	local q = dbQuery(handlerConnect, [[SELECT * FROM gc_nickcache]])
+	local nicks = dbPoll(p, -1)
+	
+	sendClientData(nicks, result, player, r)
+    --triggerClientEvent('teamsData', resourceRoot, result, player, r)
 
     -- Check if player is in a team
     if not r then
