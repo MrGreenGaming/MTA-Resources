4c181b8b8f40067c6c748ea25414162e842e4f97
AfuSensi <t.amatkarijo@gmail.com>
2019-01-18 12:13:27 +0100

    AutoLogin Tempfix - fixed typo

--- resources/[gameplay]/gc/gc_s.lua
+++ resources/[gameplay]/gc/gc_s.lua
@@ -47,7 +47,7 @@ end
 
 function onAutoLogin(forumID, player)
     removeElementData( player,'gc.autoLoginCache') -- Account Switch Bug TempFix - https://github.com/MrGreenGaming/MTA-Resources/issues/488
-    
+
     if not forumID then
         return
     end
@@ -55,7 +55,7 @@ function onAutoLogin(forumID, player)
     if not accounts[player] then
         accounts[player] = SAccount:create(player)
         accounts[player]:loginViaForumID(forumID, player, function(result, player)
-            if result then8
+            if result then
                 onLoginSuccessfull(player)
             else 
                 onLoginFailed(player, true)
