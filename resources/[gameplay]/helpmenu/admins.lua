2c70007e838b65a8751fd20f26217bab51d3491e
Cena <mohammed3103@gmail.com>
2019-01-28 22:17:54 +0100

    Removed p0l as well

--- resources/[gameplay]/helpmenu/admins.lua
+++ resources/[gameplay]/helpmenu/admins.lua
@@ -6,7 +6,6 @@ admins = {
 	{ name = "Bob_Taylor", title = "Map Manager", accountName = "Bob_Taylor" },
 	{ name = "MoshPit", title = "Map Manager", accountName = "MoshMosh" },
 	{ name = "Dubby", title = "Map Manager", accountName = "michael" },
-	{ name = "P0L", title = "Map Assistant", accountName = "P0L" },
 	{ name = "Jack123", title = "Mother Russia", accountName = "Jack123" },
 	{ name = "Stig", title = "Old Admin", accountName = "Stig" },
 	{ name = "Abrahax", title = "Kadabrahax", accountName = "abrahax" },
