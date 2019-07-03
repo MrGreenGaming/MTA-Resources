f61efa2d32033d6aeb1ef842b0632b7b6ba6cad6
Santiii733 <santiii733@outlook.com.ar>
2019-07-03 12:23:16 -0300

    Update admins.lua

--- resources/[gameplay]/helpmenu/admins.lua
+++ resources/[gameplay]/helpmenu/admins.lua
@@ -1,16 +1,15 @@
 admins = {
 	{ name = "Cena", title = "Owner", accountName = "Cena" },
+	{ name = "Kreator", title = "Server Manager", accountName = "Goldberg" },
 	{ name = "Bierbuikje", title = "Developer", accountName = "bierbuikje" },
 	{ name = "Mihoje", title = "Developer", accountName = "Mihoje" },
 	{ name = "KaliBwoy", title = "Developer", accountName = "KaliBwoy" },
-	{ name = "Bob_Taylor", title = "Map Manager", accountName = "Bob_Taylor" },
 	{ name = "MoshPit", title = "Map Manager", accountName = "MoshMosh" },
 	{ name = "Dubby", title = "Map Manager", accountName = "michael" },
 	{ name = "Jack123", title = "Mother Russia", accountName = "Jack123" },
 	{ name = "Stig", title = "Old Admin", accountName = "Stig" },
 	{ name = "Abrahax", title = "Kadabrahax", accountName = "abrahax" },
 	{ name = "Anthony~Vz", title = "Taco Admin", accountName = "AnthonyVz" },
-	{ name = "Kreator", title = "Admin", accountName = "Goldberg" },
 	{ name = "BlueYoshi", title = "Admin", accountName = "BlueYoshi97" },
 	{ name = "Jap", title = "Admin", accountName = "jap3" },
 }
