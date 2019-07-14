a7a0b6325e714759bd86eb483e81f6e55a2ee4ec
Santiii733 <santiii733@outlook.com.ar>
2019-07-13 21:07:50 -0300

    Update admins.lua

--- resources/[gameplay]/helpmenu/admins.lua
+++ resources/[gameplay]/helpmenu/admins.lua
@@ -9,7 +9,7 @@ admins = {
 	{ name = "Jack123", title = "Mother Russia", accountName = "Jack123" },
 	{ name = "Stig", title = "Old Admin", accountName = "Stig" },
 	{ name = "Abrahax", title = "Kadabrahax", accountName = "abrahax" },
-	{ name = "LeTearDuGemeaux", title = "Taco Admin", accountName = "AnthonyVz" },
+	{ name = "LeTearDuGemeaux", title = "Web Admin", accountName = "AnthonyVz" },
 	{ name = "BlueYoshi", title = "Admin", accountName = "BlueYoshi97" },
 	{ name = "Jap", title = "Admin", accountName = "jap3" },
 	{ name = "Haxardous", title = "Admin", accountName = "Axa" },
