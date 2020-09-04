f0f8294967c8e413dacae064dc04a43d0797f0c0
Cena <mohammed3103@gmail.com>
2020-09-04 21:17:20 +0400

    Changing time for event

--- resources/[gameplay]/gc/gc_s.lua
+++ resources/[gameplay]/gc/gc_s.lua
@@ -310,7 +310,7 @@ function getPlayerVip(player)
 end
 
 local holidays = {
-    { day = 4, month = 9, hour = 18 }, -- map and double gcs event
+    { day = 4, month = 9, hour = 10 }, -- map and double gcs event
     { day = 16, month = 9 }, -- anniversary
     { day = 24, month = 12, hour = 18 }, -- christmas evening
     { day = 25, month = 12 }, -- first christmas day
