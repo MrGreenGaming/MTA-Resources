d33cb11b35ef5859ac81353d7fcd236215d00673
AfuSensi <t.amatkarijo@gmail.com>
2020-10-02 14:18:35 +0200

    mapratings: Fix dbQuery syntax

--- resources/[gameplay]/mapratings2/server/RatingsFetcher.lua
+++ resources/[gameplay]/mapratings2/server/RatingsFetcher.lua
@@ -1,22 +1,22 @@
 RatingsFetcher = {}
 
 function RatingsFetcher.requestAll()
-    dbQuery(Database.getConnection(), function(qh)
+    dbQuery(function(qh)
         local res = dbPoll(qh)
         if res then
             AllMapsRatings.receiveRatingsFromDatabase(res)
         end
-    end, 'SELECT `mapresourcename`, sum(case when `rating`= 1 then 1 else 0 end) AS likes, sum(case when `rating`= 0 then 1 else 0 end) AS dislikes FROM `mapratings` GROUP BY `mapresourcename')
+    end, Database.getConnection(), 'SELECT `mapresourcename`, sum(case when `rating`= 1 then 1 else 0 end) AS likes, sum(case when `rating`= 0 then 1 else 0 end) AS dislikes FROM `mapratings` GROUP BY `mapresourcename')
 end
 
 function RatingsFetcher.requestCurrent()
     local resname = getResourceName(exports.mapmanager:getRunningGamemodeMap())
     if not resname then error("No running map to fetch") return end
-    dbQuery(Database.getConnection(), function(qh)
+    dbQuery(function(qh)
         local res = dbPoll(qh, 0)
         if res then
             CurrentMapRatings.receiveRatingsFromDatabase(resname, res)
         end
-    end, 'SELECT `forumid`, `rating` FROM `mapratings` WHERE `mapresourcename` = ?', resname)
+    end, Database.getConnection(), 'SELECT `forumid`, `rating` FROM `mapratings` WHERE `mapresourcename` = ?', resname)
 end
 
