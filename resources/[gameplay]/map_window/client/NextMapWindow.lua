477307a98a4dd3e8e0b5f0da91a76162fa24c6da
AfuSensi <t.amatkarijo@gmail.com>
2020-10-02 17:59:43 +0200

    nextMapWindow::: Add simple cache to lessen dxGetTextWidth call

--- resources/[gameplay]/map_window/client/NextMapWindow.lua
+++ resources/[gameplay]/map_window/client/NextMapWindow.lua
@@ -2,12 +2,16 @@ NextMapWindow = {}
 local g__sW, g__sH = guiGetScreenSize()
 local height = dxGetFontHeight(0.45, 'bankgothic') * 3
 local hidenext = true
+local lastWindowString = false
+local width = 0
 
 function NextMapWindow.draw()
     if hidenext then return end
     local windowString = "'" .. MapData.currentMapRatings.name .. "' L:" .. MapData.currentMapRatings.likes .. " D:" .. MapData.currentMapRatings.dislikes .. "\n" .. "'" .. MapData.nextMapRatings.name .. "' L:" .. MapData.nextMapRatings.likes .. " D:" .. MapData.nextMapRatings.dislikes
-    local width = dxGetTextWidth(windowString, 0.45, 'bankgothic')
-
+    if windowString ~= lastWindowString then
+        width = dxGetTextWidth(windowString, 0.45, 'bankgothic')
+    end
+    if width == 0 then return end
     dxDrawRectangle((g__sW-100)-(width+20), 4, width+20, height+5, tocolor(0,0,0,100),false)
     dxDrawText(windowString, (g__sW-100)-(width+20)+5, 5, (g__sW-100)-(width+20)+width+15, 5+height+5, tocolor(0, 255, 0, 255), 0.45, 'bankgothic', 'center', 'center', true)
 end
