69df5c57babe4217f9fb9b2b84f3b8872853c07e
Nick Spijker <33859387+Nick026@users.noreply.github.com>
2021-02-21 12:59:46 +0100

    Third checkpoint functionality (#737)
    * Added Third Checkpoint functionality
    
    * Forgot 2 gamemode checks
    
    * Rewritten logic behind respawning
    
    * Clears third checkpoint when client goes into spectator mode
    
    Doing this avoids a static marker in the world as long the client is spectating
    
    * Added extra check on spawn to avoid errors in log

---
+++ resources/[gameplay]/third_checkpoint/c_client.lua
@@ -0,0 +1,59 @@
+local modes = {}
+modes["Sprint"] = true
+modes["Never the same"] = true
+
+local checkpoints
+
+function onMapStart(mapInfo, mapOptions, gameOptions)
+	for i, player in ipairs(getElementsByType("player")) do
+		triggerClientEvent(player, "clearThirdCheckpoint", root)
+	end
+
+	if not modes[exports.race:getRaceMode()] then return false end
+
+	checkpoints = exports.race:getCheckPoints()
+
+	if checkpoints[3] then
+		for i, player in ipairs(getElementsByType("player")) do
+			triggerClientEvent(player, "setThirdCheckpoint", root, checkpoints[3])
+		end
+	end
+
+end
+addEventHandler("onMapStarting", getRootElement(), onMapStart)
+
+function onPlayerJoin()
+	if not modes[exports.race:getRaceMode()] then return false end
+
+	if checkpoints[3] then
+		triggerClientEvent(source, "setThirdCheckpoint", root, checkpoints[3])
+	end
+end
+addEventHandler("onPlayerJoin", root, onPlayerJoin)
+
+function onPlayerReachCheckpoint(cp)
+	if not modes[exports.race:getRaceMode()] then return false end
+
+	if checkpoints[cp + 3] then
+		triggerClientEvent(source, "setThirdCheckpoint", root, checkpoints[cp + 3], not checkpoints[cp + 4])
+	else
+		triggerClientEvent(source, "clearThirdCheckpoint", root)
+	end
+end
+addEventHandler("onPlayerReachCheckpoint", root, onPlayerReachCheckpoint)
+
+function onPlayerSpawn()
+	if not modes[exports.race:getRaceMode()] then return false end
+	setTimer(function(player)
+		local cpId = getElementData(player, "race.checkpoint")
+
+		if not cpId then return false end
+		
+		if checkpoints[cpId + 2] then
+			triggerClientEvent(player, "setThirdCheckpoint", root, checkpoints[cpId + 2], not checkpoints[cpId + 3])
+		else
+			triggerClientEvent(player, "clearThirdCheckpoint", root)
+		end
+	end, 100, 1, source)
+end
+addEventHandler("onPlayerSpawn", root, onPlayerSpawn)
