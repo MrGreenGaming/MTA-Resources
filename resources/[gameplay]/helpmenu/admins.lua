49b404b090278a0bd9e36571904fa06d34b25bef
Micha3lo <michaladamczyk25@gmail.com>
2021-02-21 17:22:01 +0100

    Screw commas

--- resources/[gameplay]/helpmenu/admins.lua
+++ resources/[gameplay]/helpmenu/admins.lua
@@ -4,7 +4,7 @@ admins = {
 	{ name = "Kreator", title = "Server Manager", accountName = "Goldberg" },
 	{ name = "Mihoje", title = "Developer", accountName = "Mihoje" },
 	{ name = "Haxardous", title = "Developer", accountName = "Axa" },
-    { name = "Nick_026", title = "Gayloper", accountName= "Nick_026" }
+    { name = "Nick_026", title = "Gayloper", accountName= "Nick_026" },
 	{ name = "MoshPit", title = "Map Manager", accountName = "MoshMosh" },
 	{ name = "Dubby", title = "Map Manager", accountName = "dubby" },
     { name = "Koolaid", title = "Map Manager", accountName = "koolaidkarlis" },
