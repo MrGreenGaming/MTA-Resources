9149ebf68dc4c7805785e12197e9a11aa28143e9
Nick Spijker <33859387+Nick026@users.noreply.github.com>
2021-03-14 11:57:11 +0100

    Toggle between 2 and 3 checkpoints - Client

--- resources/[gameplay]/third_checkpoint/c_client.lua
+++ resources/[gameplay]/third_checkpoint/c_client.lua
@@ -1,6 +1,8 @@
 local marker
 local blip
 
+local enabled = isEnabledXML()
+
 function clearThirdCheckpoint()
 	if marker then destroyElement(marker) end
 	marker = nil
@@ -19,6 +21,7 @@ addCommandHandler("afk", clearThirdCheckpoint)
 function setThirdCheckpoint(checkpoint, isFinish, facing)
 	if marker then destroyElement(marker) end
 	if blip then destroyElement(blip) end
+	if not enabled then return false end;
 
 	-- checkpoint object:
 	-- { position={x, y, z}, size=size, color={r, g, b}, type=type, vehicle=vehicleID, paintjob=paintjob, upgrades={...} } 
@@ -32,7 +35,8 @@ function setThirdCheckpoint(checkpoint, isFinish, facing)
 	end
 
 	marker = createMarker(checkpoint.position[1], checkpoint.position[2], checkpoint.position[3], checkpoint.type or "checkpoint", checkpoint.size, checkpoint.color[1], checkpoint.color[2], checkpoint.color[3] , 255)
-	
+
+
 	if facing and checkpoint.type == "ring" then
 		setMarkerTarget(marker, facing[1], facing[2], facing[3])
 	end
@@ -43,7 +47,22 @@ function setThirdCheckpoint(checkpoint, isFinish, facing)
 		icon = 53
 	else icon = 0 end
 
-	blip = createBlip(checkpoint.position[1], checkpoint.position[2], checkpoint.position[3], icon, 1, checkpoint.color[1], checkpoint.color[2], checkpoint.color[3])
+	blip = createBlip(checkpoint.position[1], checkpoint.position[2], checkpoint.position[3], icon, 1, checkpoint.color[1], checkpoint.color[2], checkpoint.color[3], 255)
 end
 addEvent("setThirdCheckpoint", true)
 addEventHandler("setThirdCheckpoint", root, setThirdCheckpoint)
+
+function toggleEnabledDisabled()
+	enabled = not enabled
+
+	if not enabled then
+		outputChatBox("#00FF00[Checkpoints]#FFFFFF You can now see 2 checkpoints", 255, 255, 255, true)
+		clearThirdCheckpoint()
+	else
+		outputChatBox("#00FF00[Checkpoints]#FFFFFF When you hit the next checkpoint you can see 3 checkpoints", 255, 255, 255, true)
+	end
+
+	saveXML(enabled)
+end
+addCommandHandler("checkpoints", toggleEnabledDisabled)
+
