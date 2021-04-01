c654bdf9705958e04e4dd55d1c5019d751178149
Nick Spijker <33859387+Nick026@users.noreply.github.com>
2021-04-01 10:23:28 +0200

    Bridgefix (#749)
    Bridge that randomly changes your position is fixed

---
+++ resources/[race]/race_fix/bridgefix.lua
@@ -0,0 +1,14 @@
+﻿local models = {
+	["13101"] = {
+		col = "countrye_3.col",
+	},
+}
+
+addEventHandler("onClientResourceStart", resourceRoot, function()
+	setTimer(function()
+		for id, data in pairs(models) do
+			local n = tonumber(id)
+			engineReplaceCOL(engineLoadCOL(data.col, n), n)
+		end
+	end, 1000, 1)
+end)
