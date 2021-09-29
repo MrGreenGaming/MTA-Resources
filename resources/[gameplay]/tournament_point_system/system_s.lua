587fe1e60d4f111192c1a408cc00c5792d7b240f
Nick026 <33859387+Nick026@users.noreply.github.com>
2021-09-29 17:52:05 +0200

    Revert "Tournament script - Prep Shooter event"
    This reverts commit 2a8228a9801844a7f42edb103b6ba58294ce8caf.

--- resources/[gameplay]/tournament_point_system/system_s.lua
+++ resources/[gameplay]/tournament_point_system/system_s.lua
@@ -149,15 +149,7 @@ function onShooterFinish(rank)
 	local playerName = getFullPlayerName(source)
 
 	if (rank <= Tournament.shooterRank) then
-
-		local pointsEarned = 0
-		if rank == 1 then
-			pointsEarned = 5
-		elseif rank == 2 then
-			pointsEarned = 3
-		elseif rank == 3 then
-			pointsEarned = 2
-		end
+		local pointsEarned = (Tournament.shooterRank - rank + 1) * 2
 
 		outputChatBox(Tournament.chatPrefix .. playerName .. " #FFFFFFfinished " .. rank .. getSuffix(rank) .. " earning " .. pointsEarned .. " points", root, 255, 255, 255, true)
 		incrementPoints(source, pointsEarned)
