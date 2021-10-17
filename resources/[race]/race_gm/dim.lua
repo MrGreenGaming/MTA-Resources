58586b7df7ab84f3586332ce1b92e43b96b0d2c5
Nick026 <33859387+Nick026@users.noreply.github.com>
2021-10-17 18:45:09 +0200

    Added /carhide command identical to pressing X

--- resources/[race]/race_gm/dim.lua
+++ resources/[race]/race_gm/dim.lua
@@ -234,6 +234,7 @@ function carhideKeyHandler(player,key,keystate)
 		outputChatBox("[CarHide] #FFFFFFVehicles hidden",player,0,255,0,true)
 	end
 end
+addCommandHandler("carhide", carhideKeyHandler)
 
 function carHideState ( state )
 	if not allowedModes[exports.race:getRaceMode()] then 
