295ed73f8c25d718ee21e3955187cf130c18bf74
Nick026 <33859387+Nick026@users.noreply.github.com>
2022-01-04 12:44:12 +0100

    Update staff list

--- resources/[gameplay]/helpmenu/admins.lua
+++ resources/[gameplay]/helpmenu/admins.lua
@@ -1,18 +1,18 @@
 admins = {
-	{ name = "KaliBwoy", title = "Owner", accountName = "KaliBwoy" },
-    { name = "Cena", title = "Co-owner", accountName = "Cena" },
-	{ name = "Kreator", title = "Server Manager", accountName = "Goldberg" },
+	{ name = "Lucker", title = "Owner", accountName = "Lucker" },
+	{ name = "Berg", title = "Server Manager", accountName = "Goldberg" },
 	{ name = "Mihoje", title = "Developer", accountName = "Mihoje" },
 	{ name = "Haxardous", title = "Developer", accountName = "Axa" },
-    { name = "Nick_026", title = "Gayloper", accountName = "Nick_026" },
+  { name = "Nick_026", title = "Gayloper", accountName = "Nick_026" },
 	{ name = "MoshPit", title = "Map Manager", accountName = "MoshMosh" },
 	{ name = "Dubby", title = "Map Manager", accountName = "dubby" },
-    { name = "Koolaid", title = "Map Manager", accountName = "koolaidkarlis" },
+  { name = "Koolaid", title = "Map Manager", accountName = "koolaidkarlis" },
 	{ name = "Stig", title = "Old Admin", accountName = "Stig" },
 	{ name = "AbueloEbrio", title = "Alcoholic Admin", accountName = "santiii733" },
 	{ name = "Jap", title = "Admin", accountName = "jap3" },
-    { name = "Mekuar", title = "Admin", accountName = "Mekuar1" },
+  { name = "Mekuar", title = "Admin", accountName = "Mekuar1" },
 	{ name = "Military", title ="Admin", accountName = "mili"},
 	{ name = "Mateoryt", title="Admin", accountName = "mateoryt"},
-	{ name = "Semert", title="Admin", accountName = "semert"}
+	{ name = "Semert", title="Admin", accountName = "semert"},
+	{	name = "Sandro", title="Admin", accountName = "Patogjeniii"}
 }
