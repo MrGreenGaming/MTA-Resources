430cc3e0f6b15aef570e966f96d46ec93058294b
Nick026 <33859387+Nick026@users.noreply.github.com>
2023-07-20 12:34:14 +0200

    Fixed wheels not applying

--- resources/[gameplay]/cw_script/server/wheels.lua
+++ resources/[gameplay]/cw_script/server/wheels.lua
@@ -32,7 +32,7 @@ addEventHandler('onElementModelChange', root, function()
     if getElementType(source)== 'vehicle' then
         local player = getVehicleOccupant(source)
         if not player then return end
-        setTimer(applyWheels, 50, 1, player, vehicle)
+        setTimer(applyWheels, 50, 1, player, source)
     end
 end)
 
