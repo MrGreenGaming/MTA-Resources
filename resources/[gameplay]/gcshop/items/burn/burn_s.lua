ec24bb4a256c8133013b38aa1fcbd6dc0e5583c4
Nick026 <33859387+Nick026@users.noreply.github.com>
2023-08-01 09:28:48 +0200

    Disable longer burnup time

--- resources/[gameplay]/gcshop/items/burn/burn_s.lua
+++ resources/[gameplay]/gcshop/items/burn/burn_s.lua
@@ -62,6 +62,8 @@ function onVehicleDamage(loss)
 		return
 	elseif not isPerkAllowedInMode(ID) then
 		return
+    elseif getResourceState(getResourceFromName("cw_script")) == "running" and exports.cw_script:areTeamsSet() then
+        return
 	elseif isElement(veh) and getElementHealth(veh) - loss < 250 then
 		busy_with_player[player] = true
 		if not g_PlayersBurnExtra[player] then
@@ -76,27 +78,27 @@ addEventHandler('onVehicleDamage', root, onVehicleDamage)
 
 function resetBurnUpTime(player, times, marker, marker1)
 	if not (isElement(player) and getPedOccupiedVehicle(player) and isElement(getPedOccupiedVehicle(player)) and getElementHealth(getPedOccupiedVehicle(player)) < 250)
-		or getElementData(player,"race.finished") or (currentRaceState and currentRaceState ~= "Running" and currentRaceState ~= "SomeoneWon") or getElementData(player, 'state', false) ~= "alive" 
+		or getElementData(player,"race.finished") or (currentRaceState and currentRaceState ~= "Running" and currentRaceState ~= "SomeoneWon") or getElementData(player, 'state', false) ~= "alive"
 		then
 		busy_with_player[player] = nil
 		if isElement(marker) then destroyElement(marker) end
 		if isElement(marker1) then destroyElement(marker1) end
 		return
 	end
-	
+
 	busy_with_player[player] = true
 	local veh = getPedOccupiedVehicle(player)
 	setElementHealth(veh, 250)
 	setTimer(function(veh) if isElement(veh) then setElementHealth(veh,1)end end, 50, 1, veh)
-	
+
 	if not (marker and marker1 and isElement(marker) and isElement(marker1)) then
 		local px, py, pz = getElementPosition(veh)
 		marker = createMarker( px, py, pz, 'corona', 2);	setElementParent(marker, veh);	attachElements(marker, veh)
 		marker1 = createMarker( px, py, pz, 'corona', 2);	setElementParent(marker1, veh);	attachElements(marker1, veh)
 	end
-	
+
 	if type(times) ~= "number" or times <= 1 then
-		setTimer ( function(player, marker, marker1) 
+		setTimer ( function(player, marker, marker1)
 			busy_with_player[player] = nil
 			if isElement(marker) then destroyElement(marker) end
 			if isElement(marker1) then destroyElement(marker1) end
@@ -104,7 +106,7 @@ function resetBurnUpTime(player, times, marker, marker1)
 	else
 		setTimer ( resetBurnUpTime, extratime, 1, player, times - 1, marker, marker1 )
 	end
-	
+
 	exports.messages:outputGameMessage('You burned a little longer! ' .. (times or ''), player)
 end
 
@@ -136,58 +138,58 @@ local function getPlayerName(player)
 end
 
 -- function onPlayersVehicleCollide (player1, car1, player2, car2)
--- 	local saveFireHealth 
+-- 	local saveFireHealth
 -- 	if g_PlayersBurnTransfer[player1] and isPerkAllowedInMode(ID) and getElementHealth(car1) <= 245 and getElementHealth(car2) > 245 and not isVehicleBlown(car1) and not isVehicleBlown(car2) then  --isOnFire?
 -- 		saveFireHealth = getElementHealth(car1)
 -- 		fixVehicle(car1)
 -- 		setElementHealth(car1, getElementHealth(car2))
 -- 		setElementHealth(car2, saveFireHealth)    --transfer the fire !
-		
+
 -- 		outputChatBox('You have taken ' .. getPlayerName(player2).. '\'s health!', player1, 50, 202, 50)
 -- 		exports.messages:outputGameMessage('You have taken ' .. getPlayerName(player2).. '\'s health!', player1, 2, 50, 202, 50, true)
-		
+
 -- 		outputChatBox(getPlayerName(player1).. ' has given you his vehicle fire! (fire transfer gc perk)', player2, 50, 202, 50)
 -- 		exports.messages:outputGameMessage(getPlayerName(player1).. ' has given you his vehicle fire! ', player2,2, 50, 202, 50, true)
-		
+
 -- 	elseif g_PlayersBurnTransfer[player2] and isPerkAllowedInMode(ID) and getElementHealth(car2) <= 245 and getElementHealth(car1) > 245 and not isVehicleBlown(car1) and not isVehicleBlown(car2) then   --isOnFire
 -- 		saveFireHealth = getElementHealth(car2)
 -- 		--fixVehicle(car2)
 -- 		--setElementHealth(car2, getElementHealth(car1))
 -- 		setElementHealth(car1, saveFireHealth)	 --transfer the fire !
-		
+
 -- 		outputChatBox('You have taken ' .. getPlayerName(player1).. '\'s health!', player2, 50, 202, 50)
 -- 		exports.messages:outputGameMessage('You have taken ' .. getPlayerName(player1).. '\'s health!', player2, 2, 50, 202, 50, true)
-		
+
 -- 		outputChatBox(getPlayerName(player2).. ' has given you his vehicle fire! ', player1, 50, 202, 50)
 -- 		exports.messages:outputGameMessage(getPlayerName(player2).. ' has given you his vehicle fire! ', player1,2, 50, 202, 50, true)
--- 	end	
+-- 	end
 -- end
 function onPlayersVehicleCollide (player1, car1, player2, car2)
-	local saveFireHealth 
+	local saveFireHealth
 	if g_PlayersBurnTransfer[player1] and isPerkAllowedInMode(ID) and getElementHealth(car1) <= 245 and getElementHealth(car2) > 245 and not isVehicleBlown(car1) and not isVehicleBlown(car2) then  --isOnFire?
 		saveFireHealth = getElementHealth(car1)
 		fixVehicle(car1)
 		setElementHealth(car1, getElementHealth(car2))
 		setElementHealth(car2, saveFireHealth)    --transfer the fire !
-		
+
 		outputChatBox('You have taken ' .. getPlayerName(player2).. '\'s health!', player1, 50, 202, 50)
 		exports.messages:outputGameMessage('You have taken ' .. getPlayerName(player2).. '\'s health!', player1, 2, 50, 202, 50, true)
-		
+
 		outputChatBox(getPlayerName(player1).. ' has given you his vehicle fire! (fire transfer gc perk)', player2, 50, 202, 50)
 		exports.messages:outputGameMessage(getPlayerName(player1).. ' has given you his vehicle fire! ', player2,2, 50, 202, 50, true)
-		
+
 	elseif g_PlayersBurnTransfer[player2] and isPerkAllowedInMode(ID) and getElementHealth(car2) <= 245 and getElementHealth(car1) > 245 and not isVehicleBlown(car1) and not isVehicleBlown(car2) then   --isOnFire
 		saveFireHealth = getElementHealth(car2)
 		fixVehicle(car2)
 		setElementHealth(car2, getElementHealth(car1))
 		setElementHealth(car1, saveFireHealth)	 --transfer the fire !
-		
+
 		outputChatBox('You have taken ' .. getPlayerName(player1).. '\'s health!', player2, 50, 202, 50)
 		exports.messages:outputGameMessage('You have taken ' .. getPlayerName(player1).. '\'s health!', player2, 2, 50, 202, 50, true)
-		
+
 		outputChatBox(getPlayerName(player2).. ' has given you his vehicle fire! ', player1, 50, 202, 50)
 		exports.messages:outputGameMessage(getPlayerName(player2).. ' has given you his vehicle fire! ', player1,2, 50, 202, 50, true)
-	end	
+	end
 end
 addEvent('onPlayersVehicleCollide', true)
 addEventHandler('onPlayersVehicleCollide', root,onPlayersVehicleCollide)
