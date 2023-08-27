706d18ffd1e6c3c160f8db6e831e30de44889b4e
Nick026 <33859387+Nick026@users.noreply.github.com>
2023-08-27 13:24:30 +0200

    Fixed modshop not applying after wheels

--- resources/[gameplay]/gcshop/modshop/modshop_s.lua
+++ resources/[gameplay]/gcshop/modshop/modshop_s.lua
@@ -630,7 +630,13 @@ function upgradeVehicle(player, forumID)
 			end
 		end
 		if i <= 16 and addUpg[i] and (i ~= 8 and i ~= 9) then
-			addVehicleUpgrade(veh, addUpg[i])
+            if i ~= 12 then
+                addVehicleUpgrade(veh, addUpg[i])
+            else
+                if getResourceState(getResourceFromName("cw_script")) ~= "running" or not exports.cw_script:areTeamsSet() then
+                    addVehicleUpgrade(veh, addUpg[i])
+                end
+            end
 		elseif i == 17 and addUpg[i] then
 			if permissions.colour[exports.race:getRaceMode()] then
 				local col1, col2, col3, col4 = getVehicleColor(veh)
