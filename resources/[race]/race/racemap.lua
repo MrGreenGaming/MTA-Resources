756f71e5d6f29e7b4ca0274412b89c914b69ca01
Nick026 <33859387+Nick026@users.noreply.github.com>
2023-08-28 14:51:55 +0200

    Race resource now reads spawnpoint rotation values
    Previously it was not possible to create spawnpoints on ramps or hills
    now it is

--- resources/[race]/race/racemap.lua
+++ resources/[race]/race/racemap.lua
@@ -39,7 +39,7 @@ end
 function RaceMap.load(res)
 	--Check if there are any .<map/>'s by using the real element system first
 	local resourceRoot = getResourceRootElement(res)
-	if #getElementsByType("spawnpoint",resourceRoot) > 0 then 
+	if #getElementsByType("spawnpoint",resourceRoot) > 0 then
 		--Spawnpoints are contained within the MTA map, therefore lets assume only MTA maps were used (removes general.ModifyOtherObjects dependency)
 		local meta = xmlLoadFile(':' .. getResourceName(res) .. '/' .. 'meta.xml')
 		if not meta then
@@ -70,7 +70,7 @@ function RaceMap.load(res)
 		outputDebugString('Error while loading ' .. getResourceName(res) .. ': no <race /> node in meta.xml', 2)
 		return false
 	end
-	
+
 	local xml = xmlLoadFile(':' .. getResourceName(res) .. '/' .. file)
 	if not xml then
 		outputDebugString('Error opening ' .. file, 2)
@@ -276,8 +276,8 @@ end
 function RaceElementMap:getAll(name, type)
 	local result = {}
 	-- Block out specific stuff
-	if name == "object" then 
-		return {} 
+	if name == "object" then
+		return {}
 	elseif name == "pickup" then
 		return self:getAll("racepickup",name)
 	end
@@ -289,7 +289,13 @@ function RaceElementMap:getAll(name, type)
 		for _,attr in ipairs(attrs) do
 			local val = getElementData(element, attr)
 			if attr == "rotation" then
-				val = val or getElementData(element, "rotZ")
+                val = val
+                    and { 0, 0, tonumber(val) } -- older maps only have rotation value (z-axis)
+                    or { -- newer maps use rotationX/Y/Z
+                        tonumber(getElementData(element, "rotX")) or 0,
+                        tonumber(getElementData(element, "rotY")) or 0,
+                        tonumber(getElementData(element, "rotZ")) or 0
+                    }
 			elseif attr == "position" then
 				val = val or { tonumber(getElementData(element, "posX")), tonumber(getElementData(element, "posY")), tonumber(getElementData(element, "posZ")) }
 			elseif val then
