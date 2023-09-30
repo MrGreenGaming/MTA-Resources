d6a3fa14c205eb126de243f38ae53b4de9a5392a
Nick026 <33859387+Nick026@users.noreply.github.com>
2023-09-30 11:05:43 +0200

    Added /addlaps command for staff to add laps to a map
    Staff can now use `/addlaps 3 6 9` to mark laps
    Checkpoint 3 marks lap 2
    Checkpoint 6 marks lap 3
    Checkpoint 9 marks lap 4

--- resources/[maps]/laps/server.lua
+++ resources/[maps]/laps/server.lua
@@ -2,11 +2,15 @@ local laps = {}
 
 local lapTimes = {}
 
+local currentMapRes
+
 function mapStarting(mapInfo, mapOptions, gameOptions)
     laps = {}
     lapTimes = {}
 
-    local lapList = get(mapInfo.resname..".laps")
+    currentMapRes = mapInfo.resname
+
+    local lapList = get(currentMapRes .. ".laps")
 
     if lapList then
         for item in string.gmatch(lapList, '([^,]+)') do
@@ -16,11 +20,13 @@ function mapStarting(mapInfo, mapOptions, gameOptions)
             end
         end
 
-        setElementData(source, "race.totalLaps", #laps + 1, true)
-
+        for i, player in ipairs(getElementsByType("player")) do
+            setElementData(player, "race.totalLaps", #laps + 1, true)
+        end
     else
-        laps = {}
-        setElementData(source, "race.totalLaps", nil, true)
+        for i, player in ipairs(getElementsByType("player")) do
+            setElementData(player, "race.totalLaps", nil, true)
+        end
     end
 
     for i, player in ipairs(getElementsByType("player")) do
@@ -28,6 +34,7 @@ function mapStarting(mapInfo, mapOptions, gameOptions)
         setElementData(player, "race.bestlap", nil, true)
     end
 end
+
 addEvent("onMapStarting")
 addEventHandler("onMapStarting", resourceRoot, mapStarting)
 
@@ -50,6 +57,55 @@ addEventHandler("onPlayerReachCheckpoint", root, function(checkpoint, time_)
     outputDebugString(lapTimes[source])
     setElementData(source, "race.bestlap", lapTimes[source], true)
 end)
+
+function addLapsToMap(playerSource, _, ...)
+    if not ... then return outputChatBox("Incorrect usage, use '/addlaps 5 9 11' to mark checkpoint 5,9 and 11 as lap checkpoints", playerSource, 255, 0,0) end
+    local args = { ... }
+
+    local cps = {}
+    for i, cp in ipairs(args) do
+        if tonumber(cp) then
+            table.insert(cps, tonumber(cp))
+        end
+    end
+
+    if not cps then return outputChatBox("Incorrect usage, use '/addlaps 5 9 11' to mark checkpoint 5,9 and 11 as lap checkpoints", playerSource, 255, 0,0) end
+
+    laps = cps
+
+    for i, player in ipairs(getElementsByType("player")) do
+        setElementData(player, "race.totalLaps", #cps + 1, true)
+        setElementData(player, "race.lap", "?", true)
+    end
+
+    local metaXml = xmlLoadFile(":" .. currentMapRes .. "/meta.xml")
+    if metaXml then
+        local settingsNode = xmlFindChild(metaXml, "settings", 0)
+        if not settingsNode then
+            settingsNode = xmlCreateChild(metaXml, "settings")
+        end
+
+        -- Remove any existing 'laps' nodes
+        local existingLapsNodes = xmlNodeGetChildren(settingsNode)
+        for _, node in ipairs(existingLapsNodes) do
+            if xmlNodeGetAttribute(node, "name") == "#laps" then
+                xmlDestroyNode(node)
+            end
+        end
+
+        local lapNode = xmlCreateChild(settingsNode, "setting")
+        xmlNodeSetAttribute(lapNode, "name", "#laps")
+        xmlNodeSetAttribute(lapNode, "value", table.concat(laps, ","))
+
+        -- Save the updated meta.xml
+        xmlSaveFile(metaXml)
+        xmlUnloadFile(metaXml)
+    end
+
+    outputChatBox("Updated and saved new laps to " .. currentMapRes, playerSource, 0, 255, 0)
+end
+addCommandHandler("addlaps", addLapsToMap, true, false)
+
 
 function findIndex(table, element)
     for i, value in ipairs(table) do
