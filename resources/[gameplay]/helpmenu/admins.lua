95213042f3b54cc94194e080df48feb644bb200e
Nick S. <33859387+Nick026@users.noreply.github.com>
2024-07-24 14:24:57 +0200

    Updated staff list

--- resources/[gameplay]/helpmenu/admins.lua
+++ resources/[gameplay]/helpmenu/admins.lua
@@ -1,10 +1,12 @@
 admins = {
-    { name = "Nick_026", title = "Dev & MTA Manager", accountName = "Nick_026" },
-    { name = "Cena", title = "MTA Manager", accountName = "cena"},
+	{ name = "Mateoryt", title="MTA Manager", accountName = "mateoryt"},
+    { name = "Z3ITM4NN", title = "MTA Manager", accountName = "Z3ITM4NN"},
+    { name = "Nick_026", title = "Developer", accountName = "Nick_026" },
 	{ name = "Haxardous", title = "Developer", accountName = "Axa" },
     { name = "Koolaid", title = "Map Manager", accountName = "koolaidkarlis" },
-	{ name = "Mateoryt", title="Map Manager", accountName = "mateoryt"},
+    { name = "Cena", title = "Admin", accountName = "cena"},
 	{ name = "Military", title ="Admin", accountName = "mili"},
 	{ name = "Sandro", title="Admin", accountName = "Patogjeniii"},
 	{ name = "Mekuar", title="Admin", accountName = "Mekuar1"},
+    { name = "Witchy", title="Admin", accountName = "Witchy"}
 }
