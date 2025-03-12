30ac0934bc97dd8120484db6ec5a9d17b60f25e8
Nick_026 <33859387+Nick026@users.noreply.github.com>
2025-03-12 09:51:08 +0100

    Added command for staff to easily modify "Classic Vehicle Change" setting
    Staff member no longer needs to access the meta.xml manually to change the setting. /cvc or /classicvehiclechange will now allow staff to change the setting.

--- resources/[maps]/laps/server.lua
+++ resources/[maps]/laps/server.lua
@@ -220,3 +220,44 @@ function upsertTagsToMap(playerSource, _, ...)
 end
 addCommandHandler("maptag", upsertTagsToMap, true, false)
 addCommandHandler("maptags", upsertTagsToMap, true, false)
+
+function toggleClassicVehicleSetting(playerSource)
+    local metaXml = xmlLoadFile(":" .. currentMapRes .. "/meta.xml")
+    if metaXml then
+        local settingsNode = xmlFindChild(metaXml, "settings", 0)
+        if not settingsNode then
+            settingsNode = xmlCreateChild(metaXml, "settings")
+        end
+
+        local settingNode
+        local existingSettings = xmlNodeGetChildren(settingsNode)
+        for _, node in ipairs(existingSettings) do
+            if xmlNodeGetAttribute(node, "name") == "#classicchangez" then
+                settingNode = node
+                break
+            end
+        end
+
+        if settingNode then
+            -- Toggle the boolean value
+            local currentValue = xmlNodeGetAttribute(settingNode, "value")
+            local newValue = (currentValue == "true") and "false" or "true"
+            xmlNodeSetAttribute(settingNode, "value", newValue)
+            outputChatBox("Toggled '#classicchangez' setting to: " .. newValue .." for " .. currentMapRes .. ". Starts working next time map is started.", playerSource, 0, 255, 0)
+        else
+            -- Create the setting if it doesn't exist
+            settingNode = xmlCreateChild(settingsNode, "setting")
+            xmlNodeSetAttribute(settingNode, "name", "#classicchangez")
+            xmlNodeSetAttribute(settingNode, "value", "true")
+            outputChatBox("Created '#classicchangez' setting with value: true for " .. currentMapRes .. ". Starts working next time map is started.", playerSource, 0, 255, 0)
+        end
+
+        -- Save the updated meta.xml
+        xmlSaveFile(metaXml)
+        xmlUnloadFile(metaXml)
+    else
+        outputChatBox("Error: Unable to load meta.xml.", playerSource, 255, 0, 0)
+    end
+end
+addCommandHandler("classicVehicleChange", toggleClassicVehicleSetting, true, false)
+addCommandHandler("cvc", toggleClassicVehicleSetting, true, false)
