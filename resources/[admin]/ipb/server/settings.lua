734cffd2b51de76712e6906f5ed4f777926dc8d3
Haxardous <30158626+Haxardous@users.noreply.github.com>
2025-03-16 12:41:08 +0000

    Update "ipb" resource. (0.2 -> 0.3.0)

---
+++ resources/[admin]/ipb/server/settings.lua
@@ -0,0 +1,45 @@
+
+local resourceName = (Resource.getThis()):getName()
+local settingPrefix = ("*%s."):format(resourceName)
+local prefixLength = settingPrefix:len()
+
+g_Settings = {
+	["SaveHighCPUResources"] = get("SaveHighCPUResources") or "true",
+	["SaveHighCPUResourcesAmount"] = get("SaveHighCPUResourcesAmount") or "10",
+	["NotifyIPBUsersOfHighUsage"] = get("NotifyIPBUsersOfHighUsage") or "50",
+	["AccessRightName"] = get("AccessRightName") or "general.http"
+}
+
+local function onResourceSettingChange(name, old, new)
+	g_Settings[name] = new
+
+	if Element.getByIndex("player", 0) then
+		triggerClientEvent("ipb.updateSetting", resourceRoot, name, new)
+	end
+end
+
+addEventHandler("onSettingChange", root,
+	function (settingName, old, new)
+		if not settingName:find(settingPrefix, 1, true) then
+			return
+		end
+
+		local shortSettingName = settingName:sub(prefixLength + 1)
+
+		if g_Settings[shortSettingName] ~= new then
+			onResourceSettingChange(shortSettingName, old and fromJSON(old) or old, new and fromJSON(new) or new)
+		end
+	end
+)
+
+addEventHandler("onPlayerResourceStart", root,
+	function (startedResource)
+		local matchingResource = (startedResource == resource)
+
+		if not matchingResource then
+			return false
+		end
+
+		triggerClientEvent(source, "ipb.syncSettings", source, g_Settings)
+	end
+)
