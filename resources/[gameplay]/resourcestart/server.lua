314dc7d65488bc768a165346dd07f50905ad0bc4
Nick_026 <33859387+Nick026@users.noreply.github.com>
2025-04-10 10:09:23 +0200

    Updated resource start to be smarter
    - Added delay to IPB resource to prevent early CPU usages warnings
    - Added disableForEvent to disable non compatible resources for the event server

--- resources/[gameplay]/resourcestart/server.lua
+++ resources/[gameplay]/resourcestart/server.lua
@@ -1,58 +1,82 @@
 function startResources()
 	local rootNode = xmlLoadFile('resources.xml', true)
+	if not rootNode then
+		outputDebugString('[Resource start] Failed to load resources.xml!', 1)
+		return
+	end
+
 	local resources = xmlNodeGetChildren(rootNode)
-	
+	local isEvent = getBool('isEvent', false)
+
 	local resourceName
 	local resourcePriority
 	local resourceRestart
-	
+	local resourceDelayed
+	local resourceDisabledForEvent
+
 	local priority = {}
 	local nonPriority = {}
 	local restart = {}
-	
-	for i,r in ipairs(resources) do
+	local delayed = {}
+	local disabledForEvent = {}
+
+	for i, r in ipairs(resources) do
 		resourceName = xmlNodeGetValue(r)
-		resourcePriority = tonumber(xmlNodeGetAttribute(r, 'priority'))
-		
-		if resourcePriority == 1 then
+		resourcePriority = tonumber(xmlNodeGetAttribute(r, 'priority')) or 0
+		resourceRestart = tonumber(xmlNodeGetAttribute(r, 'restart')) or 0
+		resourceDelayed = tonumber(xmlNodeGetAttribute(r, 'delayed')) or 0
+		resourceDisabledForEvent = tonumber(xmlNodeGetAttribute(r, 'disableForEvent')) or 0
+
+		if resourceDisabledForEvent == 1 then
+			table.insert(disabledForEvent, resourceName)
+		end
+
+		if resourceDelayed == 1 then
+			table.insert(delayed, resourceName)
+		elseif resourcePriority == 1 then
 			table.insert(priority, resourceName)
 		else
 			table.insert(nonPriority, resourceName)
 		end
 
-		resourceRestart = tonumber(xmlNodeGetAttribute(r, 'restart'))
 		if resourceRestart == 1 then
 			table.insert(restart, resourceName)
 		end
 	end
-	
-	local res
-	
-	for i,r in ipairs(priority) do
-		res = getResourceFromName(r)
-		
-		if res and getResourceState(res) ~= 'running' then
-			outputDebugString(string.format('[Resource start] Starting resource %s...', r))
-			startResource(res, true)
-		else
-			outputDebugString(string.format('[Resource start] Resource %s could not be found or is already started!', r))
+
+	local function tryStartResource(r, reason)
+		if isEvent and table.contains(disabledForEvent, r) then
+			outputDebugString(string.format('[Resource start] Skipping resource %s (disabled for event).', r))
+			return
 		end
-	end
-	
-	for i,r in ipairs(nonPriority) do
-		res = getResourceFromName(r)
-		
+
+		local res = getResourceFromName(r)
 		if res and getResourceState(res) ~= 'running' then
-			outputDebugString(string.format('[Resource start] Starting resource %s...', r))
+			outputDebugString(string.format('[Resource start] Starting resource %s (%s)...', r, reason))
 			startResource(res, true)
 		else
 			outputDebugString(string.format('[Resource start] Resource %s could not be found or is already started!', r))
 		end
 	end
 
-	for i,r in ipairs(restart) do
-		res = getResourceFromName(r)
+	for i, r in ipairs(priority) do
+		tryStartResource(r, 'priority')
+	end
 
+	for i, r in ipairs(nonPriority) do
+		tryStartResource(r, 'non-priority')
+	end
+
+	-- Handle delayed start after 30 seconds
+	setTimer(function()
+		for i, r in ipairs(delayed) do
+			tryStartResource(r, 'delayed')
+		end
+	end, 30000, 1)
+
+	-- Restart resources marked for restart
+	for i, r in ipairs(restart) do
+		local res = getResourceFromName(r)
 		if res and getResourceState(res) == 'running' then
 			outputDebugString(string.format('[Resource start] Restarting resource %s...', r))
 			restartResource(res)
@@ -61,4 +85,13 @@ function startResources()
 		end
 	end
 end
+
 addEventHandler('onResourceStart', resourceRoot, startResources)
+
+function getBool(var, default)
+	local result = get(var)
+	if not result then
+		return default
+	end
+	return result == 'true'
+end
