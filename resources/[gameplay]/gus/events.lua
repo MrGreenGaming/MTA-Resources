338c029235038df7f4ea1fbcea1b1a71c20e995c
VulpyWags <33859387+VulpyWags@users.noreply.github.com>
2025-06-30 09:07:26 +0200

    Added invalid events check

---
+++ resources/[gameplay]/gus/events.lua
@@ -0,0 +1,50 @@
+local iInvalidEventThreshold = 5
+local iCheckWindow = 5000 -- how far into the past to check for invalid events in milliseconds
+
+local tblRecentInvalidEvents = {}
+
+function remcol(str)
+	return string.gsub(str, '#%x%x%x%x%x%x', '')
+end
+
+addEventHandler("onPlayerTriggerInvalidEvent", root, function(eventName, isAdded, isRemote)
+	if isElement(source) and getElementType(source) == "player" then
+		tblRecentInvalidEvents[source] = tblRecentInvalidEvents[source] or {}
+		table.insert(tblRecentInvalidEvents[source],
+			{ tick = getTickCount(), eventName = eventName, isAdded = isAdded, isRemote = isRemote })
+		checkIllegality()
+	end
+end)
+
+function checkIllegality()
+	local now = getTickCount()
+
+	for player, events in pairs(tblRecentInvalidEvents) do
+		if not isElement(player) then -- player left
+			tblRecentInvalidEvents[player] = nil
+		else
+			-- Clean old event timestamps
+			local invalidEvents = {}
+			for _, event in ipairs(events) do
+				if now - event.tick <= iCheckWindow then
+					table.insert(invalidEvents, event)
+				end
+			end
+			tblRecentInvalidEvents[player] = invalidEvents
+
+			if #invalidEvents >= iInvalidEventThreshold then
+				banPlayer(player, true, false, true, "VulpyScript",
+					"Suspicious activity. Contact VulpyWags or appeal if false positive", 900)
+
+				if getResourceFromName("discord") and getResourceState(getResourceFromName("discord")) == "running" then
+					exports.discord:send("admin.log", {
+						log = remcol(getPlayerName(player)) ..
+								"has been banned by VulpyScript for triggering " .. #invalidEvents .. ".\n"
+								.. "Events:\n" .. table.concat(
+									table.map(invalidEvents, function(e) return e.eventName end), "\n")
+					})
+				end
+			end
+		end
+	end
+end
