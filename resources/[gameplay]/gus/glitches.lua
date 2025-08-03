e57573d7f182859dc4c6de99cb9e524f89a200bd
VulpyWags <33859387+VulpyWags@users.noreply.github.com>
2025-08-03 09:41:35 +0200

    Add logging to glitch disabler/enabler

--- resources/[gameplay]/gus/glitches.lua
+++ resources/[gameplay]/gus/glitches.lua
@@ -4,7 +4,15 @@ local glitchesData = {
 
 local function toggleGlitches()
 	for glitchName, glitchState in pairs(glitchesData) do
-		setGlitchEnabled(glitchName, glitchState)
+		if setGlitchEnabled(glitchName, glitchState) then
+			if glitchState then
+				outputDebugString("Glitch '" .. glitchName .. "' has been enabled.")
+			else
+				outputDebugString("Glitch '" .. glitchName .. "' has been disabled.")
+			end
+		else
+			outputDebugString("Failed to set glitch '" .. glitchName .. "' to " .. tostring(glitchState) .. ".")
+		end
 	end
 end
 addEventHandler("onResourceStart", resourceRoot, toggleGlitches)
